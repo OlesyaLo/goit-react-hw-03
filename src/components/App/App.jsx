30d70b1f4a3e3d9c4436cc7c8dbd4a693a6fe7fd
Olesya Lobodko <olesya.lobodko@gmail.com>
2024-09-19 00:20:33 +0200

    some changes

---
+++ src/components/App/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import ContactForm from "../ContactFrom";
+import SearchBox from "../SearchBox";
+import ContactList from "../ContactList";
+
+import './App.css';
+
+function App() {
+  useState 
+  
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+
+  )
+}
+
+export default App
